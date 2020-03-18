b79e103f98005c8b4db74a960b5c627b656b2933
Josue Rodriguez <smexyjosue@gmail.com>
2020-03-18 09:42:41 -0700

    acc migration schema to cars table

--- migrations/20200317142156_cars.js
+++ migrations/20200317142156_cars.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cars", (tbl) => {
+    //id
+    tbl.increments();
+    //vin
+    tbl
+      .string("vin", 18)
+      .notNullable()
+      .unique();
+    //make
+    tbl.string("make", 50).notNullable();
+    //model
+    tbl.string("model", 50).notNullable();
+    //mileage
+    tbl.integer("mileage").notNullable();
+    //transmission
+    tbl.string("transmission", 50);
+    //status
+    tbl.string("status", 50);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("cars");
 };
