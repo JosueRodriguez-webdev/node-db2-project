b98dd90807ea9871971293f03355a031509899f1
Josue Rodriguez <smexyjosue@gmail.com>
2020-03-18 09:41:33 -0700

    Set end points in server.js

--- api/server.js
+++ api/server.js
@@ -10,4 +10,30 @@ server.get("/", (req, res) => {
   res.send("<p>Server is running</p>");
 });
 
+server.get("/api/cars", (req, res) => {
+  db("cars")
+    .select("*")
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Error with getting cars." });
+    });
+});
+
+server.post("/api/cars", (req, res) => {
+  db("cars")
+    .insert(req.body, "id")
+    .then((response) => {
+      db("cars")
+        .where({ id: response[0] })
+        .then((response) => {
+          res.status(201).json(response);
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({ errorMessage: "Error with creating car." });
+    });
+});
+
 module.exports = server;
